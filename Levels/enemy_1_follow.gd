cb3abd2e3af190c5b45b68ffdf48bb3185b91f1e
Guillaume Hyvert <guillaume.hyvert@ik.me>
2026-03-22 10:37:23 +0100

    fixed animation

--- Levels/enemy_1_follow.gd
+++ Levels/enemy_1_follow.gd
@@ -58,25 +58,25 @@ func _process(delta):
 	elif progressConc >= 1029 && progressConc <= 1039:
 		$Enemy.stop()
 		$Enemy.play("down")
-	elif progressConc == 1152:
+	elif progressConc >= 1145 && progressConc <= 1152:
 		$Enemy.stop()
 		$Enemy.play("right")
-	elif progressConc == 1541:
+	elif progressConc >= 1535 && progressConc <= 1542:
 		$Enemy.stop()
 		$Enemy.play("up")
-	elif progressConc == 1666:
+	elif progressConc >= 1662 && progressConc <= 1670:
 		$Enemy.stop()
 		$Enemy.play("right")
-	elif progressConc == 1796:
+	elif progressConc >= 1792 && progressConc <= 1800:
 		$Enemy.stop()
 		$Enemy.play("down")
-	elif progressConc == 2118:
+	elif progressConc >= 2115 && progressConc <= 2120:
 		$Enemy.stop()
 		$Enemy.play("left")
-	elif progressConc == 2248:
+	elif progressConc >= 2245 && progressConc <= 2251:
 		$Enemy.stop()
 		$Enemy.play("down")
-	elif progressConc == 2375:
+	elif progressConc >= 2372 && progressConc <= 2378:
 		$Enemy.stop()
 		$Enemy.play("left")
 	if progress_ratio == 1:
